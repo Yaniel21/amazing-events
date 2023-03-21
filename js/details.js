8132232a1121e6b0c33d98c3b5e7484f4cfc0b25
yaniel21 <profeyani21@gmail.com>
2023-03-21 13:32:59 -0300

    renderizado-details-version-16

--- js/details.js
+++ js/details.js
@@ -1,7 +1,7 @@
 //* para generar el template  de details
 
 	//console.log([document])
-	let informacion = datos.events
+/* 	let informacion = datos.events
 	console.log(informacion)
 	 const queryString = location.search
 	 const params = new URLSearchParams(queryString)
@@ -23,4 +23,60 @@
 			<p class="lead text-center"><strong>Price: $</strong>${detalle.price}</p>
 		</div>
 	</div>
+	` */
+
+	//Modificaciones: implementacion de la api
+
+async function fetchApi(){
+	try{
+		let urlApi = "https://mindhub-ab35.onrender.com/api/amazing-events"
+		let fetchResponse = await fetch(urlApi)//se espera la respuesta 
+		//console.log(fetchResponse)
+
+		let response = await fetchResponse.json()//se espera la transformación
+		//console.log(response)
+		let datos = response.events
+		
+		//console.log(datos)
+		printDetails(datos)
+		
+	} catch(error){
+		console.log("ocurrio un error que diosito lo ayude")
+		console.log(error)
+	}
+}
+fetchApi()
+
+
+//console.log([document])
+
+
+function printDetails(datos){
+		//console.log([document])
+			//let informacion = datos.events
+			//console.log(informacion)
+	 		const queryString = location.search
+	 		const params = new URLSearchParams(queryString)
+	 		const id = params.get("id")
+	 		//console.log(id)
+			
+			const detalle = datos.find(each => each.id == id)
+						
+			const contenedorDetalle = document.querySelector("#tarjetaDetalle")
+						
+			contenedorDetalle.innerHTML =  `
+			<div class="row flex-lg-row-reverse align-items-center g-2 py-2 ">
+			<div class="col-12  col-lg-6  align-items-center">
+			<img src="${detalle.image}" class="d-block mx-lg-auto img-fluid sombreado" alt="${detalle.name}"
+				width="700" height="500" loading="lazy">
+		</div>
+		<div class="col-lg-6">
+			<h1 class="display-6 lh-1 mb-3 title-center d-md-flex justify-content-md-center">${detalle.name}</h1>
+			<p class="lead text-start  fst-italic m-1"><strong>Date:</strong> ${detalle.date}</p>
+			<p class="lead title-center  fw-bolder fst-italic colorDescription">${detalle.description}</p>
+			<p class="lead text-center fst-italic"><strong>Category: </strong>${detalle.category}    <strong>Place: </strong>${detalle.place} </p>
+			<p class="lead text-center"><strong>Price: $</strong>${detalle.price}</p>
+		</div>
+	</div>
 	`
+}
--- js/past.js
+++ js/past.js
@@ -1,4 +1,4 @@
-function printPastEvents() {
+/* function printPastEvents() {
 	let events = datos.events;
 	let currentDate = datos.currentDate;
 	let templates = [];
@@ -12,5 +12,68 @@ function printPastEvents() {
 	cardPast.innerHTML = templates.join("");
 	
 }
- printPastEvents();
- //Modificaciones
+ printPastEvents(); */
+
+
+ //Modificaciones implementacion de la API
+ 
+let cards = []
+
+//definimos el template para cada una de las cards dinámicas
+
+function defineTemplateCardPast(cards){
+	return `
+	<div class="col">
+	<div class="card shadow-sm">
+			<figure class="figure">
+					<img src="${cards.image}" alt="${cards.name}"  class="bd-placeholder-img card-img-top tamanioFoto">
+			</figure>
+		<div class="card-body">
+				<h1 class="d-flex flex-wrap justify-content-center font-title">${cards.name}</h1>
+				<p class="card-text font-paragraph">${cards.description}</p>
+				<div class="d-flex justify-content-between align-items-center">
+						<small class="text-muted fs-6">Price $${cards.price}</small>
+						<button class="button-color" id="boton">
+						<a href="./details.html?id=${cards.id}&nombre=${cards.name}"   class="nav-link btn btn-sm text-light button-color">Ver mas</a>
+						</button>
+				</div>
+		</div>
+	</div>
+</div>`;
+}
+
+//Modificaciones: implementacion de la api
+
+async function fetchApi(){
+	try{
+		let urlApi = "https://mindhub-ab35.onrender.com/api/amazing-events"
+		let fetchResponse = await fetch(urlApi)//se espera la respuesta 
+		//console.log(fetchResponse)
+
+		let response = await fetchResponse.json()//se espera la transformación
+		//console.log(response)
+		let datos = response.events
+		let currentDate = response.currentDate;
+		//console.log(datos)
+		printPastEvents(datos, currentDate)
+	} catch(error){
+		console.log("ocurrio un error que diosito lo ayude")
+		console.log(error)
+	}
+}
+fetchApi()
+
+function printPastEvents(datos, currentDate) {
+	let templates = [];
+
+	for (let element of datos) {
+		if (element.date < currentDate) {
+			//template(element.image, element.name,element.description,element.price )			
+		templates.push(
+			defineTemplateCardPast(element)
+			);
+		}
+	}
+	let tarjeta = document.getElementById("pastCard");
+	tarjeta.innerHTML = templates.join("");
+}
